9ec48504fbfd2f9a24b2255e3ae9af9b39055857
Pkepowicz <paw.kepowicz@gmail.com>
2022-08-17 13:55:04 +0200

    Cleared hanoi tower lab

--- Hanoi tower/main.py
+++ Hanoi tower/main.py
@@ -1,22 +1,24 @@
 from math import pow
-from collections import deque
 from time import time
+from texttable import Texttable
 
 TOWER_SIZE = 4
 
+
 ###########################################################
-#           PODEJSCIE REKURENCYJNE
+#           RECURSIVE APPROACH
 def hanoi_Rec(n, sour, s, dest, d, buff, b, moves) -> None:
     if n == 1:
-        dest.insert(0,sour.pop(0))
-        moves.append('Z ' + s + " przeniesiono na " + d)
+        dest.insert(0, sour.pop(0))
+        moves.append('From ' + s + " moved to " + d)
     else:
         hanoi_Rec(n - 1, sour, s, buff, b, dest, d, moves)
         dest.insert(0, sour.pop(0))
-        moves.append('Z ' + s + " przeniesiono na " + d)
+        moves.append('From ' + s + " moved to " + d)
         hanoi_Rec(n - 1, buff, b, dest, d, sour, s, moves)
 
-file = open('odpowiedzi_rek.txt', 'w')
+
+file = open('rec.txt', 'w')
 source = [i for i in range(1, TOWER_SIZE + 1)]
 destination, buffer = [], []
 moves = []
@@ -25,74 +27,69 @@ s, d, b = '1', '2', '3'
 hanoi_Rec(TOWER_SIZE, source, s, destination, d, buffer, b, moves)
 for i in moves:
     file.write(i + '\n')
-file.write('Wystarczylo: ' + str(len(moves)) + ' ruchow')
+file.write('Solved in ' + str(len(moves)) + ' moves')
 file.close()
 
+
 ############################################################
-#           PODEJSCIE ITERACYJNE
-def hanoi_Iter(n, sour:list, dest:list, buff:list, moves) -> None:
-    if n % 2 == 0: #zamiana wiezy docelowej
-        buff, dest = dest, buff
-        dt, bf = '3', '2'
+#           ITERATIVE APPROACH
+def move(A: list, B: list) -> bool:
+    if not A:
+        A.insert(0, B.pop(0))
+        return False
+    elif not B:
+        B.insert(0, A.pop(0))
+        return True
     else:
-        dt, bf = '2', '3'
+        if A[0] < B[0]:
+            B.insert(0, A.pop(0))
+            return True
+        else:
+            A.insert(0, B.pop(0))
+            return False
+
+
+def hanoi_Iter(n, sour: list, dest: list, buff: list, moves: list) -> None:
+    if n % 2 == 0:
+        buff, dest = dest, buff
+        sc, dt, bf = '1', '3', '2'
+    else:
+        sc, dt, bf = '1', '2', '3'
     for i in range(1, int(pow(2, n))):
-        if i%3 == 1:
-            if len(dest) == 0:
-                dest.insert(0, sour.pop(0))
-                moves.append("Z 1 przeniesiono na " + dt)
-            elif len(sour) == 0:
-                sour.insert(0, dest.pop(0))
-                moves.append("Z " + dt + " przeniesiono na 1")
-            elif sour[0] < dest[0]:
-                dest.insert(0, sour.pop(0))
-                moves.append("Z 1 przeniesiono na " + dt)
-            elif dest[0] < sour[0]:
-                sour.insert(0, dest.pop(0))
-                moves.append("Z " + dt + " przeniesiono na 1")
-        if i%3 == 2:
-            if len(buff) == 0:
-                buff.insert(0, sour.pop(0))
-                moves.append("Z 1 przeniesiono na " + bf)
-            elif len(sour) == 0:
-                sour.insert(0, buff.pop(0))
-                moves.append("Z " + bf + " przeniesiono na 1")
-            elif buff[0] < sour[0]:
-                sour.insert(0, buff.pop(0))
-                moves.append("Z " + bf + " przeniesiono na 1")
-            elif sour[0] < buff[0]:
-                buff.insert(0, sour.pop(0))
-                moves.append("Z 1 przeniesiono na " + bf)
-        if i%3 == 0:
-            if len(dest) == 0:
-                dest.insert(0, buff.pop(0))
-                moves.append("Z " + bf + " przeniesiono na " + dt)
-            elif len(buff) == 0:
-                buff.insert(0, dest.pop(0))
-                moves.append("Z " + dt + " przeniesiono na " + bf)
-            elif buff[0] < dest[0]:
-                dest.insert(0, buff.pop(0))
-                moves.append("Z " + bf + " przeniesiono na " + dt)
-            elif dest[0] < buff[0]:
-                buff.insert(0, dest.pop(0))
-                moves.append("Z " + dt + " przeniesiono na " + bf)
+        if i % 3 == 1:
+            if move(sour, dest):
+                moves.append("From " + sc + " moved to " + dt)
+            else:
+                moves.append("From " + dt + " moved to " + sc)
+        if i % 3 == 2:
+            if move(sour, buff):
+                moves.append("From " + sc + " moved to " + bf)
+            else:
+                moves.append("From " + bf + " moved to " + sc)
+        if i % 3 == 0:
+            if move(buff, dest):
+                moves.append("From " + bf + " moved to " + dt)
+            else:
+                moves.append("From " + dt + " moved to " + bf)
         i += 1
 
-file = open('odpowiedzi_iter.txt', 'w')
+
+file = open('iter.txt', 'w')
 source = [i for i in range(1, TOWER_SIZE + 1)]
 destination, buffer, moves = [], [], []
 
 hanoi_Iter(TOWER_SIZE, source, destination, buffer, moves)
 for i in moves:
     file.write(i + '\n')
-file.write('Wystarczylo: ' + str(len(moves)) + ' ruchow')
+file.write('Solved in ' + str(len(moves)) + ' moves')
 file.close()
 
 ###########################################################
-#           CZAS WYKONYWANIA
+#           Times
 ITERATIONS = [3, 4, 6, 9, 10, 12, 15, 16, 20]
 times_iter, times_rec = [], []
-file = open("czasy.txt", 'w')
+table = Texttable()
+table.add_row(["Height", "Iterative time", "Recursive time"])
 
 for i in ITERATIONS:
     src = [l for l in range(1, i + 1)]
@@ -107,6 +104,8 @@ for i in ITERATIONS:
     hanoi_Iter(i, src, dt, bf, mv)
     times_iter.append(time() - start)
 
-file.write("Czasy wykonywania algorytmow: \n wysokosc wiezy \t czas iteracyjny \t czas rekurencyjny \n")
 for i in range(len(ITERATIONS)):
-    file.write(str(ITERATIONS[i]) + '\t' + str(times_iter[i]) + '\t' + str(times_rec[i]) + '\n')
+    table.add_row([ITERATIONS[i], times_iter[i], times_rec[i]])
+
+f = open("times.txt", "w")
+f.write(table.draw())
